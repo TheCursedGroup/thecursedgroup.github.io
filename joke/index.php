45d400f3d49f9e8c5bba02c20629893cd6b59632
Bacon_Space <thespaceguild@gmail.com>
2023-09-26 17:41:09 -0400

    tcg

---
+++ joke/index.php
@@ -0,0 +1,55 @@
+<?php
+echo '
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TheCursedGroup - Joke Page</title>
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@Bacon_Space">
+    <meta name="twitter:title" content="TheCursedGroup - Joke PAge">
+    <meta name="twitter:description" content="Made By Open AI And Bacon_Space">
+    <meta name="twitter:image" content="https://i.imgur.com/62s95F1.png">
+    <link rel="icon" type="image/x-icon" href="https://i.imgur.com/62s95F1.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+  </head>
+  <body>
+
+';
+?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">TheCursedGroup</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="http://thecursedgroup.rf.gd/joke/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./pun/">Puns</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./dad/">Dad</a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="./chuck/">Chuck</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+    <div class="container">
+      <h1>Welcome to TheCursedGroup - DadJoke</h1>
+      <p>Made By Open AI And Bacon_Space</p>
+    </div>
+    
+    <script src="https://unpkg.com/@popperjs/core@2.10.1/dist/esm/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/js/bootstrap.min.js"></script>
+  </body>
+</html>
